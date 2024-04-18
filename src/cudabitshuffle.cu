0fd7e3804803a3239f1bd59cba953034c25dc352
Dimitri Vlachos <dimitri.j.vlachos@gmail.com>
2024-04-18 15:30:32 +0100

    Add CUDA error detection

--- src/cudabitshuffle.cu
+++ src/cudabitshuffle.cu
@@ -5,7 +5,23 @@
 #include <stdio.h>
 #include "cudabitshuffle.h"
 
-__global__ void cuda_bitshuffle(unsigned int *d_input, unsigned int *d_output, int numElements, int numBits) {
+// 
+inline auto cuda_error_string(cudaError_t err) {
+    const char *err_name = cudaGetErrorName(err);
+    const char *err_str = cudaGetErrorString(err);
+    printf("CUDA Error: %s: %s\n", err_name, err_str);
+}
+/// Raise an exception IF CUDA is in an error state, with the name and description
+inline auto cuda_throw_error() -> void {
+    auto err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        printf("CUDA Error: %s: %s\n", cudaGetErrorName(err), cudaGetErrorString(err));
+    }
+}
+
+
+__global__
+void cuda_bitshuffle(unsigned int *d_input, unsigned int *d_output, int numElements, int numBits) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < numElements) {
         unsigned int input = d_input[idx];
@@ -18,13 +34,12 @@ __global__ void cuda_bitshuffle(unsigned int *d_input, unsigned int *d_output, i
 }
 
 __global__ 
-void test(const char *msg) {
-    printf("Hello from CUDA: %s\n", msg);
+void test() {
+    printf("Hello from CUDA\n");
 }
 
 void run_test() {
-    printf("Hello from run_test\n");
-    test<<<1, 1>>>("Hello from kernel");
-    printf("Hello from run_test after kernel\n");
+    test<<<1, 1>>>();
+    cuda_throw_error();
     cudaDeviceSynchronize();
 }
