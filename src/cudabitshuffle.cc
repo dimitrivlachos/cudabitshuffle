d9106e33e847a918ddaf097ee87377bf87bd5073
Dimitri Vlachos <dimitri.j.vlachos@gmail.com>
2024-04-19 16:10:22 +0100

    Add pre-commit formatting

--- src/cudabitshuffle.cc
+++ src/cudabitshuffle.cc
@@ -1,11 +1,11 @@
-#include <iostream>
 #include "cudabitshuffle.h"
 #include "h5read.h"
+#include <iostream>
 
 int main() {
-    std::cout << "Running test:" << std::endl;
+  std::cout << "Running test:" << std::endl;
 
-    run_test();
+  run_test();
 
-    return 0;
+  return 0;
 }
--- src/cudabitshuffle.cu
+++ src/cudabitshuffle.cu
@@ -1,45 +1,43 @@
 /*
  * CUDA Kernel for bit shuffling
-*/
+ */
 
-#include <stdio.h>
 #include "cudabitshuffle.h"
+#include <stdio.h>
 
-// 
+//
 inline auto cuda_error_string(cudaError_t err) {
-    const char *err_name = cudaGetErrorName(err);
-    const char *err_str = cudaGetErrorString(err);
-    printf("CUDA Error: %s: %s\n", err_name, err_str);
+  const char *err_name = cudaGetErrorName(err);
+  const char *err_str = cudaGetErrorString(err);
+  printf("CUDA Error: %s: %s\n", err_name, err_str);
 }
-/// Raise an exception IF CUDA is in an error state, with the name and description
+/// Raise an exception IF CUDA is in an error state, with the name and
+/// description
 inline auto cuda_throw_error() -> void {
-    auto err = cudaGetLastError();
-    if (err != cudaSuccess) {
-        printf("CUDA Error: %s: %s\n", cudaGetErrorName(err), cudaGetErrorString(err));
+  auto err = cudaGetLastError();
+  if (err != cudaSuccess) {
+    printf("CUDA Error: %s: %s\n", cudaGetErrorName(err),
+           cudaGetErrorString(err));
+  }
+}
+
+__global__ void cuda_bitshuffle(unsigned int *d_input, unsigned int *d_output,
+                                int numElements, int numBits) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx < numElements) {
+    unsigned int input = d_input[idx];
+    unsigned int output = 0;
+    for (int i = 0; i < numBits; i++) {
+      output |= ((input >> i) & 1) << (numBits - 1 - i);
     }
+    d_output[idx] = output;
+  }
 }
 
-
-__global__
-void cuda_bitshuffle(unsigned int *d_input, unsigned int *d_output, int numElements, int numBits) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < numElements) {
-        unsigned int input = d_input[idx];
-        unsigned int output = 0;
-        for (int i = 0; i < numBits; i++) {
-            output |= ((input >> i) & 1) << (numBits - 1 - i);
-        }
-        d_output[idx] = output;
-    }
-}
-
-__global__ 
-void test() {
-    printf("Hello from CUDA\n");
-}
+__global__ void test() { printf("Hello from CUDA\n"); }
 
 void run_test() {
-    test<<<1, 1>>>();
-    cuda_throw_error();
-    cudaDeviceSynchronize();
+  test<<<1, 1>>>();
+  cuda_throw_error();
+  cudaDeviceSynchronize();
 }
